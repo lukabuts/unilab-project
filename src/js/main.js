9617d7143f769f92c08bb9fa0f735f5cadf2d7a8
Luka Butskhrikidze <butskhrikidzeluka0@gmail.com>
2023-11-03 00:14:42 +0400

    User info appears after clicking on user img

--- src/js/main.js
+++ src/js/main.js
@@ -89,3 +89,13 @@ previousNewsBtn.addEventListener('click', ()=>{
     generateArticles();
     nextNewsBtn.disabled = false;
 })
+
+
+
+/* Appearing User info after clicking user img */
+const userImg  = document.getElementById('user-img');
+const userInfo = document.getElementById('user-info');
+
+userImg.addEventListener('click', () => {
+    userInfo.classList.toggle('active')
+})

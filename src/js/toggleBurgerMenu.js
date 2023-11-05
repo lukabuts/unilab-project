d6ee257a335b54f6b695756e2281a4b8891dc0e3
Luka Butskhrikidze <butskhrikidzeluka0@gmail.com>
2023-11-05 11:49:23 +0400

    Added dialog to burger button

--- src/js/toggleBurgerMenu.js
+++ src/js/toggleBurgerMenu.js
@@ -1,11 +1,11 @@
 const burgerBtn = document.getElementById('burger-btn');
+const mobileNavbarDialog = document.getElementById('mobile-navbar-dialog');
 const mobileNavbar = document.getElementById('mobile-navbar');
-const closer = document.getElementById('closer');
 const exitBtn = document.getElementById('exitBtn');
 
 /* Open function */
 burgerBtn.addEventListener('click', function(){
-    mobileNavbar.classList.add("active");
+    mobileNavbarDialog.showModal();
 })
 
 /* Close function triggers */
@@ -13,11 +13,14 @@ exitBtn.addEventListener('click', exitNavbar);
 window.addEventListener('resize', () => {
     if(window.innerWidth >= 465) exitNavbar();
 })
-window.addEventListener('click', (e) => {
-    if(e.clientX > mobileNavbar.clientWidth) exitNavbar();
+
+mobileNavbarDialog.addEventListener('click', (e) => {
+    if(e.clientX > mobileNavbar.clientWidth){
+        exitNavbar();
+    }
 })
 
  /* Exiting mobile-nav menu */
  function exitNavbar(){
-    mobileNavbar.classList.remove("active");
+    mobileNavbarDialog.close();
 }
